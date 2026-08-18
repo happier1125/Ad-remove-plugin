5fc272a4225c838e323be89dab7e76d60f7aa995
Liano <124172922+happier1125@users.noreply.github.com>
2026-08-19 00:47:39 +0800

    Update GitHub token to placeholder
    Replaced hardcoded GitHub token with a placeholder.

--- github_star_release_monitor_loon.js
+++ github_star_release_monitor_loon.js
@@ -33,7 +33,7 @@ const CONFIG = {
     // Account permissions
     //     Starring -> Read-only
     //
-    GITHUB_TOKEN: "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",
+    GITHUB_TOKEN: "Your GitHub Token",
 
 
     // ========================================================
